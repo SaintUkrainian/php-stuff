33b6ade4ca9770b57eac1fdda73372968304d547
Matsenko Denys <idanchik47@gmail.com>
2020-11-24 22:38:17 +0200

    min password length is 6 characters now

--- laba2/signup.php
+++ laba2/signup.php
@@ -43,7 +43,7 @@
         </div>
         <div class="form-group">
             <label for="exampleInputPassword1">Password</label>
-            <input type="password" name="password" class="form-control" id="exampleInputPassword1" required>
+            <input type="password" name="password" class="form-control" id="exampleInputPassword1" minlength="6" required>
             <div class="valid-feedback">
                 Looks good!
             </div>
